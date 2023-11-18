0fb0364639c6a72afcda962c39374dd90316e891
Drukast <132137309+Drukast@users.noreply.github.com>
2023-11-18 17:03:17 +0700

    matrix creation

--- task2.js
+++ task2.js
@@ -2,5 +2,11 @@ var M, N, matrix = [], min = 999, col = 0;
 M = Number(prompt("Введите Кол-во Строк", ""));
 N = Number(prompt("Введите Кол-во столбцов", ""));
 if ((!(isNaN(M)) && (M % 1 == 0) && (M > 0)) && (!(isNaN(N)) && (N % 1 == 0) && (N > 0))) {
-
+    for (var i = 0; i < M; i++) {
+        let submatrix = [];
+        for (var j = 0; j < N; j++) {
+            submatrix.push(Math.round((Math.random() * 100)));
+        }
+        matrix.push(submatrix);
+    }
 }
